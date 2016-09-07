172f46c4de6999ebc5a5f243aacf03cf51d9f517
Denis O <denis@universal-development.com>
2016-09-07 15:50:37 +0300

    Tests on storage loading

--- polydata-flat-file-storage/src/test/java/com/unidev/polydata/TestFlatFileStorage.java
+++ polydata-flat-file-storage/src/test/java/com/unidev/polydata/TestFlatFileStorage.java
@@ -5,6 +5,14 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.nullValue;
 
 /**
  * Test flat file operations storage
@@ -21,9 +29,23 @@ public class TestFlatFileStorage {
         flatFileStorage.getList().add(basicPoly);
 
         File tempFile = File.createTempFile("storage", ".json");
-        //tempFile.deleteOnExit();
+        tempFile.deleteOnExit();
 
         FlatFileStorageMapper.storageMapper().saveSource(tempFile).save(flatFileStorage);
+    }
+
+    @Test
+    public void testStorageLoading() throws MalformedURLException, URISyntaxException {
+        InputStream inputStream = TestFlatFileStorage.class.getResourceAsStream("/test.json");
+        FlatFileStorage storage = FlatFileStorageMapper.storageMapper().loadSource(inputStream).load();
+
+        assertThat(storage, is(not(nullValue())));
+        assertThat(storage.getMetadata(), is(not(nullValue())));
+        assertThat(storage.getMetadata()._id(), is ("metadata_id"));
+
+        assertThat(storage.list().size(), is(2));
+        assertThat(storage.getList().get(1).link(), is("tomato"));
+        assertThat(storage.getList().get(1).fetch("custom_key"), is("custom_value"));
     }
 
 }
