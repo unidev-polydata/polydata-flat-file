8ebaae1816c0905dd9b92b0b80b83017d52c9de8
Denis O <denis@universal-development.com>
2017-06-01 21:33:51 +0300

    Added flat file storage object

--- polydata-flat-file-storage/src/main/java/com/unidev/polydata/FlatFileStorage.java
+++ polydata-flat-file-storage/src/main/java/com/unidev/polydata/FlatFileStorage.java
@@ -1,107 +1,33 @@
 package com.unidev.polydata;
 
-
-import com.unidev.polydata.domain.BasicPoly;
-import com.unidev.polydata.domain.Poly;
-import com.unidev.polydata.storage.ChangablePolyStorage;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import com.unidev.polydata.domain.bucket.BasicPolyBucket;
 
 /**
  * Flat file poly storage, storage is separated in metadata poly and list of records
  */
-public class FlatFileStorage implements ChangablePolyStorage {
+public class FlatFileStorage  {
 
-    private BasicPoly metadata;
-    private List<BasicPoly> list;
+    private BasicPolyBucket polyBucket;
+    private String file;
 
     public FlatFileStorage() {
-        metadata = new BasicPoly();
-        list = new ArrayList<>();
+
     }
 
-    public FlatFileStorage(BasicPoly metadata, List<BasicPoly> polyList) {
-        this.metadata = metadata;
-        this.list = polyList;
+    public BasicPolyBucket getPolyBucket() {
+        return polyBucket;
     }
 
-    @Override
-    public <P extends Poly> P persist(P poly) {
-        list.add((BasicPoly) poly);
-        return poly;
+    public void setPolyBucket(BasicPolyBucket polyBucket) {
+        this.polyBucket = polyBucket;
     }
 
-    @Override
-    public boolean remove(String id) {
-        Poly poly = fetchById(id);
-        if (poly != null) {
-            list.remove(poly);
-            return true;
-        }
-        return false;
+    public String getFile() {
+        return file;
     }
 
-    @Override
-    public <P extends Poly> P metadata() {
-        return (P) metadata;
+    public void setFile(String file) {
+        this.file = file;
     }
 
-    @Override
-    public <P extends Poly> P fetchById(String id) {
-        for(Poly poly : list) {
-            if (id.equalsIgnoreCase(poly._id())) {
-                return (P) poly;
-            }
-        }
-        return null;
-    }
-
-    @Override
-    public Collection<? extends Poly> list() {
-        return list;
-    }
-
-    @Override
-    public long size() {
-        return list.size();
-    }
-
-    public boolean hasPoly(String id) {
-        if (fetchById(id) != null) {
-            return true;
-        }
-        return false;
-    }
-
-    public FlatFileStorage add(BasicPoly poly) {
-        persist(poly);
-        return this;
-    }
-
-    public FlatFileStorage addFirst(BasicPoly poly) {
-        list.add(0, poly);
-        return this;
-    }
-
-    public void withMetadata(BasicPoly metadata) {
-        this.metadata = metadata;
-    }
-
-    public List<BasicPoly> getList() {
-        return list;
-    }
-
-    public void setList(List<BasicPoly> list) {
-        this.list = list;
-    }
-
-    public BasicPoly getMetadata() {
-        return metadata;
-    }
-
-    public void setMetadata(BasicPoly metadata) {
-        this.metadata = metadata;
-    }
 }
