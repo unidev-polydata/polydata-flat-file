8e43de4994cacd2b3c07837691ab995badffcd11
Denis O <denis@universal-development.com>
2019-08-10 14:58:10 +0300

    Basic storage operations

--- polydata-storage-flat-file-json/src/main/java/com/unidev/polydata/FlatFileStorage.java
+++ polydata-storage-flat-file-json/src/main/java/com/unidev/polydata/FlatFileStorage.java
@@ -2,54 +2,78 @@ package com.unidev.polydata;
 
 import com.unidev.polydata.domain.*;
 import com.unidev.polydata.domain.bucket.BasicPolyBucket;
+import com.unidev.polydata.model.FlatFileModel;
 import com.unidev.polydata.storage.PolyStorage;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
 /**
  * Flat file poly storage, storage is separated in metadata poly and list of records
  */
+@AllArgsConstructor
+@NoArgsConstructor
 public class FlatFileStorage implements PolyStorage {
 
+    private FlatFileModel flatFileModel;
 
     @Override
     public <P extends Poly> Optional<P> metadata(String container) {
-        return Optional.empty();
+        return (Optional<P>) Optional.ofNullable(flatFileModel.getMetadata().get(container));
     }
 
     @Override
     public <P extends Poly> Optional<P> fetchById(String container, String id) {
-        return Optional.empty();
+        HashMap<String, BasicPoly> mapByContainer = flatFileModel.getData().get(container);
+        if (mapByContainer == null) {
+            return Optional.empty();
+        }
+        return (Optional<P>) Optional.ofNullable(mapByContainer.get(id));
+
     }
 
     @Override
     public <P extends Poly> P persist(String container, P poly) {
-        return null;
+        HashMap<String, BasicPoly> mapByContainer = flatFileModel.getData().get(container);
+        mapByContainer.put(poly._id(), (BasicPoly) poly);
+        return poly;
     }
 
     @Override
     public <P extends Poly> P persistIndex(String container, Map<String, Object> keys, P poly) {
-        return null;
+        throw new FlatFileStorageException("Index persisting not supported");
     }
 
     @Override
     public <P extends Poly> P persistMetadata(String container, P metadata) {
-        return null;
+        flatFileModel.getMetadata().put(container, (BasicPoly) metadata);
+        return metadata;
     }
 
     @Override
     public <P extends PolyList> P query(String container, PolyQuery polyQuery) {
-        return null;
+        throw new FlatFileStorageException("Query not supported");
     }
 
     @Override
     public <P extends PolyList> P queryIndex(String container, PolyQuery polyQuery) {
-        return null;
+        throw new FlatFileStorageException("Query index not supported");
     }
 
     @Override
     public boolean removePoly(String container, String id) {
-        return false;
+        HashMap<String, BasicPoly> mapByContainer = flatFileModel.getData().get(container);
+        if (mapByContainer == null) {
+            return false;
+        }
+        if (!mapByContainer.containsKey(id)) {
+            return false;
+        }
+        mapByContainer.remove(id);
+
+        return true;
     }
 }
---
+++ polydata-storage-flat-file-json/src/main/java/com/unidev/polydata/model/FlatFileModel.java
@@ -0,0 +1,27 @@
+package com.unidev.polydata.model;
+
+import com.unidev.polydata.domain.BasicPoly;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashMap;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class FlatFileModel {
+
+    /**
+     * container:metadata
+     */
+    private HashMap<String, BasicPoly> metadata = new HashMap<>();
+
+    /**
+     * {container: data{id: poly}}
+     */
+    private HashMap<String, HashMap<String, BasicPoly>> data = new HashMap<>();
+
+}
